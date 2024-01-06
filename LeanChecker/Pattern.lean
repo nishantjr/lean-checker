3ca0db7b09a23a83ee35c1b11fed6275cf4d5402
Nishant Rodrigues <nishantjr@gmail.com>
2024-01-06 16:24:45 +0530

    Pattern: Implement `instantiate`

--- LeanChecker/Pattern.lean
+++ LeanChecker/Pattern.lean
@@ -4,8 +4,29 @@ inductive Pattern where
     | bot : Pattern
     | implies(left: Pattern)(right: Pattern) : Pattern
 
+def implies := Pattern.implies
+
 def ph0 : Pattern := (Pattern.metavar 0)
-def ph0_implies_ph0 : Pattern := (Pattern.implies ph0 ph0)
+def ph1 : Pattern := (Pattern.metavar 1)
+def ph0_implies_ph0 : Pattern := (implies ph0 ph0)
+
+def Pattern.instantiate(p: Pattern)(id: Nat)(plug: Pattern) : Pattern :=
+    match p with
+      | Pattern.metavar id' => if id' = id then plug else p
+      | Pattern.bot => Pattern.bot
+      | Pattern.implies left right => Pattern.implies (instantiate left id plug) (instantiate right id plug)
+
+theorem test_inst_1 :
+    (Pattern.instantiate Pattern.bot 0 ph0) = Pattern.bot := by rfl
+theorem test_inst_2 :
+    (Pattern.instantiate ph0 0 ph0) = ph0 := by rfl
+theorem test_inst_3 :
+    (Pattern.instantiate ph1 0 ph0) = ph1 := by rfl
+theorem test_inst_4 :
+    (Pattern.instantiate ph0_implies_ph0 0 ph1) = (implies ph1 ph1) := by rfl
+theorem test_inst_5 :
+    (Pattern.instantiate ph0_implies_ph0 0 ph0_implies_ph0) = (implies ph0_implies_ph0 ph0_implies_ph0) := by rfl
+
 
 inductive Proof where
     | instantiate(schema: Proof)(id: Nat)(value: Pattern) : Proof /-- TODO: We need simultanous substitutions here --/
