4e78e40c208531f01bb5c3d4e5a3f72612918e33
Nishant Rodrigues <nishantjr@gmail.com>
2024-02-15 18:42:08 -0500

    partial proof for instantiate_commutes_with_mp

--- LeanChecker/Proof.lean
+++ LeanChecker/Proof.lean
@@ -14,6 +14,8 @@ def mp := Proof.modus_ponens
 
 def prop1_concl := (implies ph0 (implies ph1 ph0))
 def prop2_concl := (implies (implies ph0 (implies ph1 ph2)) (implies (implies ph0  ph1) (implies ph0 ph2)))
+
+@[simp]
 def mp_conclusion(hyp1: Option Pattern)(hyp2: Option Pattern) : Option Pattern := match (hyp1, hyp2) with
           | (_, none) => none
           | (none, _) => none
@@ -21,6 +23,7 @@ def mp_conclusion(hyp1: Option Pattern)(hyp2: Option Pattern) : Option Pattern :
                 if concl_2_left = concl_1 then concl_2_right else none
           | (some _, some _) => none
 
+@[simp]
 def Proof.conclusion(pi: Proof) : Option Pattern :=
     match pi with
       | Proof.instantiate pi' subst' =>
@@ -40,3 +43,36 @@ def imp_refl: Proof :=
     )
 
 theorem test_imp_refl : imp_refl.conclusion = some ph0_implies_ph0 := by rfl
+
+
+theorem instantiate_commutes_with_implies(left: Pattern)(right: Pattern)(subst: List (Nat × Pattern))
+    : ((Pattern.implies left right).instantiate subst) = (Pattern.implies (left.instantiate subst) (right.instantiate subst))
+    := by rw [Pattern.instantiate]
+theorem instantiate_commutes_with_mp_happy(left: Proof)(right: Proof)(subst: List (Nat × Pattern))
+    : left.conclusion = (Pattern.implies phi psi)
+   -> right.conclusion = some phi
+   -> (Proof.instantiate (Proof.modus_ponens left right) subst).conclusion
+    = (Proof.modus_ponens (Proof.instantiate left subst) (Proof.instantiate right subst)).conclusion
+    := by {
+    intro h_left_conc
+    intro h_right_conc
+    simp [  mp_conclusion, h_right_conc, h_left_conc, instantiate_commutes_with_implies]
+    }
+theorem instantiate_commutes_with_mp(left: Proof)(right: Proof)(subst: List (Nat × Pattern))
+    : (Proof.instantiate (Proof.modus_ponens left right) subst).conclusion
+    = (Proof.modus_ponens (Proof.instantiate left subst) (Proof.instantiate right subst)).conclusion
+    := by {
+        generalize h_left_conc: left.conclusion = lconc
+        generalize h_right_conc: right.conclusion = rconc
+        match lconc with
+        | none  => simp [h_left_conc]
+        | some (Pattern.implies phi psi)  => {
+            match rconc with
+            | none => simp [h_left_conc, h_right_conc]
+            | some phi' => sorry --- Need to do case analysis on if `phi == phi'`
+                                 --- In the first case, apply instantiate_commutes_with_mp_happy
+                                 --- in the second, the proof should reduce to `none = none`
+        }
+        | some _  => sorry -- This is strictly more general than the previous case
+                           -- However, we want to assume that it is *not* an implication
+    }
